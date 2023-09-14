362fd3bebc7bad18ee201d8e136f0e7152924502
goat james <129680954+goatjames69@users.noreply.github.com>
2023-09-14 19:34:20 +0545

    Add files via upload

---
+++ jamesxd/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace jamesxd
+{
+    internal class Program
+    {
+        static  void Main(string[] args)
+        {
+            Console.Title = "Hwid Extractor - by Jamesxd";
+            string hwid;
+            hwid = System.Security.Principal.WindowsIdentity.GetCurrent().User.Value;
+            Console.WriteLine("Searching Your Hwid\n");
+            Task.Delay(1000).Wait();
+            Console.WriteLine("Fetching Your Hwid...\n");
+            Task.Delay(1000).Wait();
+            Console.WriteLine("Hwid Found!\n");
+            Console.WriteLine("Hwid: "+hwid);
+            Task.Delay(20000000).Wait();
+            Task.Delay(20000000).Wait();
+            Task.Delay(20000000).Wait();
+            Task.Delay(20000000).Wait();
+            Task.Delay(20000000).Wait();
+            Clipboard.SetText(hwid);
+        }
+    }
+}
